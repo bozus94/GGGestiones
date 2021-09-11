152d32955a2ed64155607c312c79425929f73843
bozus94 <bj_ortega@hotmail.com>
2021-09-11 19:05:21 +0200

    agregada la funcion localstorage

--- Resources/components/cuadre-item.js
+++ Resources/components/cuadre-item.js
@@ -4,17 +4,26 @@ Vue.component("cuadre-item", {
 			type: String,
 		}
 	},
+	created() {
+		console.log(`cuadre${this.cuadre}`);
+		const data = JSON.parse(localStorage.getItem(`cuadre${this.cuadre}`));
+		if (data !== null) {
+			this.datas = data;
+		}
+	},
 	data() {
 		return {
-			billetes: "",
-			monedas: "",
-			pendientes: "",
-			total: 0,
-			caja: "",
-			mensaje: {
-				text: "Introduzca los datos",
-				bg: 'alert-secondary'
-			},
+			datas: {
+				billetes: "", 
+				monedas: "",
+				pendientes: "",
+				total: 0,
+				caja: "",
+				mensaje: {
+					text: "Introduzca los datos",
+					bg: 'alert-secondary'
+				},
+			}
 		};
 	},
 	computed: {
@@ -22,44 +31,45 @@ Vue.component("cuadre-item", {
 	},
 	methods: {
 		calculate() {
-			if (this.caja == '' || null) {
-				this.mensaje.text = 'La caja no pueder estar vacia';
-				this.mensaje.bg = 'alert-secondary';
+			if (this.datas.caja == '' || null) {
+				this.datas.mensaje.text = 'La caja no pueder estar vacia';
+				this.datas.mensaje.bg = 'alert-secondary';
 				return false;
-			} else if(this.caja === 0 || this.caja === '0') {git 
-				this.mensaje.text = 'La caja no puede ser igual al 0'
-				this.mensaje.bg = 'alert-secondary'
+			} else if(this.datas.caja === 0 || this.datas.caja === '0') {
+				this.datas.mensaje.text = 'La caja no puede ser igual al 0'
+				this.datas.mensaje.bg = 'alert-secondary'
 			} else{	
 				this.verifyAttributes();
+				localStorage.setItem(`cuadre${this.cuadre}`, JSON.stringify(this.datas))
 
-				this.total = parseFloat(this.billetes) + parseFloat(this.monedas) + parseFloat(this.pendientes);
-				let diferencia = this.total - this.caja;
-				if (this.total < this.caja) {
-					this.mensaje.text = `Te faltan ${this.decimales(diferencia)}€`;
-					this.mensaje.bg = 'alert-danger'
+				this.datas.total = parseFloat(this.datas.billetes) + parseFloat(this.datas.monedas) + parseFloat(this.datas.pendientes);
+				let diferencia = this.datas.total - this.datas.caja;
+				if (this.datas.total < this.datas.caja) {
+					this.datas.mensaje.text = `Te faltan ${this.decimales(diferencia)}€`;
+					this.datas.mensaje.bg = 'alert-danger'
 				}
-				if (this.total == this.caja) {
-					this.mensaje.text = `Muy bien, Tienes cuadrado!`;
-					this.mensaje.bg = 'alert-success'
+				if (this.datas.total == this.datas.caja) {
+					this.datas.mensaje.text = `Muy bien, Tienes cuadrado!`;
+					this.datas.mensaje.bg = 'alert-success'
 					
 				}
-				if (this.total > this.caja) {
-					this.mensaje.text = `Muy bien, Tienes cuadrado! y te sobran ${this.decimales(diferencia)}€`;
-					this.mensaje.bg = 'alert-success'
+				if (this.datas.total > this.datas.caja) {
+					this.datas.mensaje.text = `Muy bien, Tienes cuadrado! y te sobran ${this.decimales(diferencia)}€`;
+					this.datas.mensaje.bg = 'alert-success'
 				}
 			}
 			
 		},
 		decimales: (number) => Math.abs(number.toFixed(2)),
 		verifyAttributes() {
-			if (this.billetes === '' || this.billetes === null) {
-				this.billetes = 0;
+			if (this.datas.billetes === '' || this.datas.billetes === null) {
+				this.datas.billetes = 0;
 			}
-			if (this.monedas === '' || this.monedas === null) {
-				this.monedas = 0;
+			if (this.datas.monedas === '' || this.datas.monedas === null) {
+				this.datas.monedas = 0;
 			}
-			if (this.pendientes === '' || this.pendientes === null) {
-				this.pendientes = 0;
+			if (this.datas.pendientes === '' || this.datas.pendientes === null) {
+				this.datas.pendientes = 0;
 			}
 		},
 	},
@@ -71,7 +81,7 @@ Vue.component("cuadre-item", {
 				<div class="row row-cols-2 mx-auto my-3 justify-content-center align-content-center">
 					<div class="col-6">
 						<div class="form-floating mb-3">
-							<input type="text" @keyup="calculate" v-model="caja" class="form-control" placeholder="caja" />
+							<input type="text" @keyup="calculate" v-model="datas.caja" class="form-control" placeholder="caja" />
 							<label for="caja">Caja</label>
 						</div>
 					</div>
@@ -79,26 +89,26 @@ Vue.component("cuadre-item", {
 				<div class="row row-cols-2 mx-auto my-3 justify-content-md-center align-content-center">
 					<div class="col-4">
 						<div class="form-floating mb-3">
-							<input type="text" @keyup="calculate" v-model="billetes" class="form-control" placeholder="billetes" />
+							<input type="text" @keyup="calculate" v-model="datas.billetes" class="form-control" placeholder="billetes" />
 							<label for="billetes"> Billetes</label>
 						</div>
 					</div>
 					<div class="col-4">
 						<div class="form-floating mb-3">
-							<input type="text" @keyup="calculate" v-model="monedas" class="form-control" placeholder="monedas" />
+							<input type="text" @keyup="calculate" v-model="datas.monedas" class="form-control" placeholder="monedas" />
 							<label for="monedas"> Monedas</label>
 						</div>
 					</div>
 					<div class="col-4">
 						<div class="form-floating mb-3">
-							<input typea="text" @keyup="calculate" v-model="pendientes" class="form-control" placeholder="pendientes" />
+							<input typea="text" @keyup="calculate" v-model="datas.pendientes" class="form-control" placeholder="pendientes" />
 							<label for="pendientes">Pendientes</label>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="alert text-center fs-5 mb-0" role="alert" :class="[mensaje.bg]">
-				{{mensaje.text}}
+			<div class="alert text-center fs-5 mb-0" role="alert" :class="[datas.mensaje.bg]">
+				{{datas.mensaje.text}}
 			</div>
 		</div>
 		
